bd2536fc10be1af0895a2d5e8d8783f5d9abe9e5
Pat Cataldo <cataldo71@gmail.com>
2021-10-06 11:16:07 -0400

    Cognito configuration

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-2_eY6ptIMOA', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'vivr6h04n6lf64v48drsu5dkk', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
